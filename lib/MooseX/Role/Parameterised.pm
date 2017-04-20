721b0f1b1e805229c9a29d260d0588fa6fc9c736
Karen Etheridge <ether@cpan.org>
2017-04-20 10:20:45 -0700

    add MooseX::Role::Parameterised (thanks bigpresh!)

---
+++ lib/MooseX/Role/Parameterised.pm
@@ -0,0 +1,16 @@
+use strict;
+use warnings;
+package MooseX::Role::Parameterised;
+# vim: set ts=8 sts=4 sw=4 tw=115 et :
+# ABSTRACT: Moose roles with composition parameters
+
+our $VERSION = '1.10';
+
+__END__
+
+=head1 SYNOPSIS
+
+See L<MooseX::Role::Parameterized>; this module is just a stub to help the
+civilised Perl users find this distribution with search engines. :)
+
+=cut
